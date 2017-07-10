7d89b63c7bd2f4efcb8da6c8007faa1881a02a5b
Dang Quoc Huy <huysamdua@yahoo.com>
2017-07-10 23:01:32 +0700

    Step 2: Schedule a Notification

--- LocalNotification/ViewController.swift
+++ LocalNotification/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
 
@@ -20,6 +21,20 @@ class ViewController: UIViewController {
     // Dispose of any resources that can be recreated.
   }
 
+  @IBAction func tapSendNotification(_ sender: Any) {
+    // notification content
+    let content = UNMutableNotificationContent()
+    content.title = "Huy said:"
+    content.body = "This is a notification!"
+    content.sound = UNNotificationSound.default()
+    content.badge = NSNumber(value: UIApplication.shared.applicationIconBadgeNumber + 1)
+    content.categoryIdentifier = "huy.dang.quoc.localnotification"
+    // deliver in 10 seconds
+    let trigger = UNTimeIntervalNotificationTrigger.init(timeInterval: 10, repeats: false)
+    let request = UNNotificationRequest.init(identifier: "10seconds", content: content, trigger: trigger)
+    // Schedule the notification.
+    let center = UNUserNotificationCenter.current()
+    center.add(request)
+  }
 
 }
-
